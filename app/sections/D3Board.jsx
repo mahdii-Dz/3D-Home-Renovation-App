744538701169ea59da18596f72b953f73eabb795
mahdi <hadjidjmahdi@gmail.com>
2025-06-25 23:10:45 +0100

    adding loader for better UX

--- app/sections/D3Board.jsx
+++ app/sections/D3Board.jsx
@@ -1,9 +1,10 @@
 'use client'
-import React, { useContext, useMemo } from 'react'
+import React, { Suspense, useContext, useMemo } from 'react'
 import { Canvas, useLoader } from '@react-three/fiber'
 import { OrbitControls, PerspectiveCamera } from '@react-three/drei'
 import * as THREE from 'three'
 import { GlobalContext } from '../context/Context'
+import CanvasLoader from '../components/CanvasLoader'
 
 // Get wall end point from rotation and width
 const getWallEnd = (wall) => {
@@ -20,6 +21,7 @@ function WallMesh({ wall, offsetX, offsetY }) {
   const wallThickness = 0.1
   const angleRad = THREE.MathUtils.degToRad(wall.rotate)
   const end = getWallEnd(wall)
+
 
   // Offset and scale to meters
   const x1 = (wall.left - offsetX) / 100
@@ -89,26 +91,28 @@ function D3Board() {
       shadows
       style={{ width: '100%', height: '100vh', background: '#ffffff', marginTop: '100px' }}
     >
-      {/* Camera */}
-      <PerspectiveCamera makeDefault position={[0, 10, 10]} fov={50} />
-      <OrbitControls target={bounds.centerVec3} />
+      <Suspense fallback={<CanvasLoader/>}>
+        {/* Camera */}
+        <PerspectiveCamera makeDefault position={[0, 10, 10]} fov={50} />
+        <OrbitControls target={bounds.centerVec3} />
 
-      {/* Lighting */}
-      <ambientLight intensity={0.3} />
-      <directionalLight
-        castShadow
-        position={[10, 20, 10]}
-        intensity={1.5}
-        shadow-mapSize-width={2048}
-        shadow-mapSize-height={2048}
-      />
-      <hemisphereLight intensity={0.4} />
+        {/* Lighting */}
+        <ambientLight intensity={0.3} />
+        <directionalLight
+          castShadow
+          position={[10, 20, 10]}
+          intensity={1.5}
+          shadow-mapSize-width={2048}
+          shadow-mapSize-height={2048}
+        />
+        <hemisphereLight intensity={0.4} />
 
-      {/* Floor and walls */}
-      <RoomFloor bounds={bounds} />
-      {walls.map((wall, index) => (
-        <WallMesh key={index} wall={wall} offsetX={bounds.offsetX} offsetY={bounds.offsetY} />
-      ))}
+        {/* Floor and walls */}
+        <RoomFloor bounds={bounds} />
+        {walls.map((wall, index) => (
+          <WallMesh key={index} wall={wall} offsetX={bounds.offsetX} offsetY={bounds.offsetY} />
+        ))}
+      </Suspense>
     </Canvas>
   )
 }
