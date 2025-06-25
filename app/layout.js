9746b7df690245811ece900b21afcbce2ef26d96
mahdi <hadjidjmahdi@gmail.com>
2025-06-25 22:16:10 +0100

    seo optimize

--- app/layout.js
+++ app/layout.js
@@ -9,12 +9,34 @@ const roboto = Roboto({
 export const metadata = {
   title: "Renovate IT",
   description: "Generated A 3D model for your home",
+  openGraph: {
+    title: "Renovate IT",
+    description: "Generated A 3D model for your home",
+    url: "https://renovate-it.netlify.app/",
+    images: [
+      {
+        url: "/Renovate-IT.png",
+        width: 1366,
+        height: 768,
+      },
+    ],
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "Renovate IT",
+    description:
+      "Generated A 3D model for your home",
+    creator: "@mahdii_Dz",
+    image: ["/Renovate-IT.png",],
+  },
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={`${roboto.variable} overflow-hidden w-full h-dvh`}>{children}</body>
+      <body className={`${roboto.variable} overflow-hidden w-full h-dvh`}>
+        {children}
+      </body>
     </html>
   );
 }
